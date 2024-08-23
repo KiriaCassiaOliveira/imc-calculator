a6c800b38a7515c107a32e3eb1da009c7cf41ffc
KiriaCassiaOliveira <kiriacsoliveira@gmail.com>
2024-08-23 10:35:38 -0300

    feat: structuring html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
-
-
 function App() {
-
   return (
     <main>
-      <h1 className="bg-red-500">Calculadora IMC</h1>
+      <section id="form">
+        <form>
+          <div>
+            <label htmlFor="weight">Peso (kg)</label>
+            <input type="text" id="weight" />
+          </div>
+          <div>
+            <label htmlFor="height">Altura (cm)</label>
+            <input type="text" id="height" />
+          </div>
+          <button>Calcular</button>
+        </form>
+      </section>
+      <section id="result">
+        <p>Saiba agora se está no seu peso ideal!</p>
+      </section>
+      <section id="reference-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Imc</th>
+              <th>Classificação</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+              <td>Menor que 17</td>
+              <td>Muito abaixo do peso</td>
+            </tr>
+            <tr>
+              <td>Menos de 18.5</td>
+              <td>Abaixo do peso</td>
+            </tr>
+            <tr>
+              <td>Entre 18.5 e 24.9</td>
+              <td>Peso normal</td>
+            </tr>
+            <tr>
+              <td>Entre 25 e 29.9</td>
+              <td>Sobrepeso</td>
+            </tr>
+            <tr>
+              <td>Entre 30 e 39.9</td>
+              <td>Obesidade grau 1</td>
+            </tr>
+            <tr>
+              <td>Acima de 40</td>
+              <td>Obesidade grau 2</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
